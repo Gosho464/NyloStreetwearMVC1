eeb0882f1bd37173f5dce041768398fd82f6e9cd
Gosho464 <georgi_buzhev@abv.bg>
2026-03-21 12:52:06 +0200

    add product create or edit view model

---
+++ NyloStreetwearMVC1/Models/Product/ProductCreateOrEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NyloStreetwearMVC1.Models.Product
+{
+    public class ProductCreateOrEditViewModel
+    {
+        public int Id { get; set; } // For Edit, will be 0 for Create
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public List<int> SelectedCategoryIds { get; set; } = new List<int>();   
+    }
+}
